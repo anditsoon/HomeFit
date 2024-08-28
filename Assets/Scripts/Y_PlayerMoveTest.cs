4f680a904dc00b4fe9ccf48e149ef59bc6087980
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-08-28 18:34:53 +0900

    맵 만듦

---
+++ Assets/Scripts/Y_PlayerMoveTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Y_PlayerMoveTest : MonoBehaviour
+{
+    float v;
+    float h;
+    Vector3 dir;
+    float moveSpeed = 7;
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        dir = new Vector3(h, 0, v);
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
+    }
+}
