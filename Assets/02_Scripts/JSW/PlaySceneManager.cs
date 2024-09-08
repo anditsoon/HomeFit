09fa1b2ff1609b2d5ec1f3342207c90965092cc1
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-09-08 21:17:05 +0900

    last Prototype

--- Assets/02_Scripts/JSW/PlaySceneManager.cs
+++ Assets/02_Scripts/JSW/PlaySceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlaySceneManager : MonoBehaviour
 {
@@ -12,5 +13,10 @@ public class PlaySceneManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MoveMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
