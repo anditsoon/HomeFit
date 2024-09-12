56e4359c83ba2106147d9d95c962392eede7ae8b
SongMinJe <dlsgur3543@gmail.com>
2024-09-12 18:40:22 +0900

    kakaotest_1

--- Assets/02_Scripts/SMJ/UserInfoManager.cs
+++ Assets/02_Scripts/SMJ/UserInfoManager.cs
@@ -10,8 +10,8 @@ public class UserInfoManager : MonoBehaviour
     public Button confirmButton;
     private const int id = 8;
     //TODO : 고정 IP 나오면 변경
-    private readonly string TokenUrl = "http://192.168.0.183:8080/api/auth/kakao/login";
-    private readonly string UserInfoUrl = $"http://192.168.0.183:8080/api/user/{id}";
+    private readonly string TokenUrl = "http://192.168.0.76:8081/api/auth/kakao/login";
+    private readonly string UserInfoUrl = $"http://192.168.0.76:8081/api/user/{id}";
 
     void Start()
     {
@@ -24,29 +24,6 @@ public class UserInfoManager : MonoBehaviour
         GetTokenAndSendUserInfo();
     }
 
-    /*IEnumerator GetTokenAndSendUserInfo()
-    {
-        // 1. GET 요청으로 토큰 받아오기
-        using (UnityWebRequest tokenRequest = UnityWebRequest.Get(TokenUrl))
-        {
-            yield return tokenRequest.SendWebRequest();
-
-            if (tokenRequest.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError("토큰 받기 실패: " + tokenRequest.error);
-                yield break;
-            }
-            else
-            {
-                string token = tokenRequest.downloadHandler.text;
-                Debug.Log("받은 토큰: " + token);
-                // 클라 상에서 playerprefab으로 저장
-                PlayerPrefs.SetString("jwtToken", token);
-                // 2. 받은 토큰과 사용자 정보를 함께 POST로 전송
-                yield return StartCoroutine(SendUserInfo(token));
-            }
-        }
-    }*/
     public void GetTokenAndSendUserInfo()
     {
         StartCoroutine(HttpClient.Instance.Get(TokenUrl,
@@ -54,7 +31,7 @@ public class UserInfoManager : MonoBehaviour
             {
                 string token = result;
                 // 클라 상에서 playerprefab으로 저장
-                PlayerPrefs.SetString("jwtToken", token);
+                
                 // 2. 받은 토큰과 사용자 정보를 함께 POST로 전송
                 SendUserInfo(token);
             },
@@ -65,42 +42,11 @@ public class UserInfoManager : MonoBehaviour
          ));
     }
 
-    
-
-    /*IEnumerator SendUserInfo(string token)
-    {
-        print(token);
-        WWWForm form = new WWWForm();
-        form.AddField("jwtToken", token);
-        form.AddField("nickname", AvatarInfo.instance.NickName);
-        form.AddField("height", AvatarInfo.instance.Height.ToString());
-        form.AddField("weight", AvatarInfo.instance.Weight.ToString());
-        form.AddField("birthday", AvatarInfo.instance.Birthday.ToString());
-
-        using (UnityWebRequest www = UnityWebRequest.Post(UserInfoUrl, form))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError("사용자 정보 전송 실패: " + www.error);
-            }
-            else
-            {
-                string result = www.downloadHandler.text;
-                Debug.Log("사용자 정보 전송 성공. 서버 응답: " + result);
-                // 씬이동
-                OnFullWindow();
-                SceneManager.LoadScene("avatarScene");
-            }
-        }
-    }*/
-
     IEnumerator SendUserInfo(string token)
     {
         print(token);
         WWWForm form = new WWWForm();
-        form.AddField("jwtToken", token);
+        form.AddField("token", token);
         form.AddField("nickname", AvatarInfo.instance.NickName);
         form.AddField("height", AvatarInfo.instance.Height.ToString());
         form.AddField("weight", AvatarInfo.instance.Weight.ToString());
