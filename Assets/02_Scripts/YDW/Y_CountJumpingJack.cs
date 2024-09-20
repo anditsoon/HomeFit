a763f06dee2a0b5499f85bf77889e1edea167830
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-09-21 05:33:24 +0900

    fix

--- Assets/02_Scripts/YDW/Y_CountJumpingJack.cs
+++ Assets/02_Scripts/YDW/Y_CountJumpingJack.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Y_CountJumpingJack : MonoBehaviour
+public class Y_CountJumpingJack : MonoBehaviour, IPunObservable
 {
     public bool startGame;
     public bool isJumpingJack;
     public float jumpingJackCount;
+    PhotonView pv;
     Y_MediaPipeTest mediapipe;
     Transform leftHandPos;
     Transform rightHandPos;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(jumpingJackCount);
+        }
+        // 그렇지 않고, 만일 데이터를 서버로부터 읽어오는 상태라면...
+        else if (stream.IsReading)
+        {
+            jumpingJackCount = (float)stream.ReceiveNext();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +34,7 @@ public class Y_CountJumpingJack : MonoBehaviour
         mediapipe = GetComponent<Y_MediaPipeTest>();
         leftHandPos = mediapipe.leftArmTarget.transform;
         rightHandPos = mediapipe.rightArmTarget.transform;
+        pv = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -36,15 +52,21 @@ public class Y_CountJumpingJack : MonoBehaviour
             //print("손 좌표 : " + leftHandPos.position.y + " 오른손도!! : " + rightHandPos.position.y);
             if (leftHandPos.position.y > 4.8f && rightHandPos.position.y > 4.8f && !isJumpingJack)
             {
-                jumpingJackCount++;
-                isJumpingJack = true;
+                if (pv.IsMine)
+                {
+                    jumpingJackCount++;
+                    isJumpingJack = true;
+                }
                 //print("!!!!!!!! 손 좌표 : " + leftHandPos.position.y + " 오른손도!! : " + rightHandPos.position.y);
             }
 
             if (leftHandPos.position.y < 3.7f && rightHandPos.position.y < 3.7f && isJumpingJack)
             {
-                isJumpingJack = false;
-                //print("?????????/ 손 좌표 : " + leftHandPos.position.y + " 오른손도!! : " + rightHandPos.position.y);
+                if (pv.IsMine)
+                {
+                    isJumpingJack = false;
+                    //print("?????????/ 손 좌표 : " + leftHandPos.position.y + " 오른손도!! : " + rightHandPos.position.y);
+                }
             }
 
             print("점핑잭 횟수: " + jumpingJackCount);
