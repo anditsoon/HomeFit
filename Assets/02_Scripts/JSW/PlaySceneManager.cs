81d2ced3f171d0166a68b4d47cdade96b98d2d9b
SEOKWON <50188612+seokwonJ@users.noreply.github.com>
2024-09-24 16:56:06 +0900

    fix

--- Assets/02_Scripts/JSW/PlaySceneManager.cs
+++ Assets/02_Scripts/JSW/PlaySceneManager.cs
@@ -22,20 +22,20 @@ public class PlaySceneManager : MonoBehaviourPunCallbacks
 
     private Hashtable CP;
 
-    //private void Awake()
-    //{
-    //    if (instance == null)
-    //    {
-    //        instance = this;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
 
-    //        // 씬 전환이 되도 게임 오브젝트를 파괴하고 싶지않다.
-    //        DontDestroyOnLoad(gameObject);
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
+            // 씬 전환이 되도 게임 오브젝트를 파괴하고 싶지않다.
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void MoveMainScene()
     {
