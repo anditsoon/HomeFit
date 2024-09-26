b35255009900eadf27a5a5fc95ac4e7e16cdbec6
SongMinJe <dlsgur3543@gmail.com>
2024-09-26 17:04:17 +0900

    ProfileDelay_1

--- Assets/02_Scripts/JSW/CalenderImage.cs
+++ Assets/02_Scripts/JSW/CalenderImage.cs
@@ -1,93 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine.UI;
+
 public class CalenderImage : MonoBehaviour
 {
-    public Button btn_Cal;
-    GameObject profileManager;
-    TMP_Text CalenderDay;
+    public Button dateButton;
+    private TMP_Text dateText;
+    private CalendarManager calendarManager;
 
-    TMP_Text ExerciseCount;
-    TMP_Text KcalCount;
-    TMP_Text MinutesCount;
-
-    float ExerciseCountNum=0;
-    float KcalCountNum=0;
-    float MinutesCountNum=0;
-
-    float NowExerciseCountNum = 0;
-    float NowKcalCountNum = 0;
-    float NowMinutesCountNum = 0;
-
-    private void Start()
+    private void Awake()
     {
-        profileManager = GameObject.Find("ProfileUIManager");
+        dateButton = GetComponent<Button>();
+        dateText = GetComponent<TMP_Text>();
+        calendarManager = FindObjectOfType<CalendarManager>();
 
-        CalenderDay = GameObject.Find("CalenderDay").GetComponent<TMP_Text>();
-
-        ExerciseCount = GameObject.Find("ExerciseCount").transform.GetChild(0).GetComponent<TMP_Text>();
-        KcalCount = GameObject.Find("KcalCount").transform.GetChild(0).GetComponent<TMP_Text>();
-        MinutesCount = GameObject.Find("MinutesCount").transform.GetChild(0).GetComponent<TMP_Text>();
-
-        // 첫시작 즉 이때 오늘 날짜에 해당하는 값 넣어 주면됨 
-        ExerciseCountNum = AvatarInfo.instance.totalExerciseCount;
-        KcalCountNum = (float)AvatarInfo.instance.totalCaloriesBurned;
-        MinutesCountNum = ExerciseCountNum * 0.5f;
-
-        btn_Cal = GetComponent<Button>();
-        btn_Cal.onClick.AddListener(() =>
+        if (calendarManager == null)
         {
-            SetBoldText();
-        });
+            Debug.LogError("CalendarManager not found in the scene!");
+        }
+
+        dateButton.onClick.AddListener(OnDateClicked);
     }
 
-    private void Update()
+    private void OnDateClicked()
     {
-        if (GetComponent<TMP_Text>().fontStyle != FontStyles.Bold)
-        {
-            return;
-        }
-        NowExerciseCountNum = ExerciseCountNum;
-        ExerciseCount.text = NowExerciseCountNum.ToString();
-        NowKcalCountNum = (int)Mathf.Ceil(Mathf.Lerp(NowKcalCountNum, KcalCountNum, Time.deltaTime * 5));
-        KcalCount.text = NowKcalCountNum.ToString();
-        NowMinutesCountNum = (int)Mathf.Ceil(Mathf.Lerp(NowMinutesCountNum, MinutesCountNum, Time.deltaTime * 0.001f));
-        MinutesCount.text = NowMinutesCountNum.ToString();
+        calendarManager.OnDateSelected(this);
     }
 
-    public void SetBoldText()
+    public string GetDateString()
     {
-        // 그날 날짜들을 눌렀을 때 정보 업데이트
-        JSWSoundManager.Get().PlayEftSound(JSWSoundManager.ESoundType.EFT_PROFILESCENE);
-        profileManager.GetComponent<ProfileUIManager>().CalenderReset();
-        GetComponent<TMP_Text>().fontStyle = FontStyles.Bold;
-        GetComponent<TMP_Text>().fontSize = 45;
-        string num = GetComponent<TMP_Text>().text;
-        if (num.Length == 1)
-        {
-            CalenderDay.text = "2024-09-0" + num;
-        }
-        else
-        {
-            CalenderDay.text = "2024-09-" + num;
-        }
-        NowExerciseCountNum = 0;
-        NowKcalCountNum = 0;
-        NowMinutesCountNum = 0;
-
-        print(NowExerciseCountNum);
-
-        // 그 날 날짜에 해당하는 수치 이때 넣어주면 됨
-        ExerciseCountNum = AvatarInfo.instance.totalExerciseCount;
-        KcalCountNum = (float) AvatarInfo.instance.totalCaloriesBurned;
-        MinutesCountNum = ExerciseCountNum * 0.5f;
-
-        GameObject.Find("Profile").GetComponent<Animator>().enabled = true;
-        GameObject.Find("Profile").GetComponent<Animator>().CrossFade("ProfileUIUI",0);
+        return $"2024-09-{dateText.text.PadLeft(2, '0')}";
     }
 
-    
+    public void SetBold(bool isBold)
+    {
+        dateText.fontStyle = isBold ? FontStyles.Bold : FontStyles.Normal;
+        dateText.fontSize = isBold ? 45 : 40; // Adjust font size as needed
+    }
 }
---
+++ Assets/02_Scripts/SMJ/CalendarManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class CalendarManager : MonoBehaviour
+{
+    public TMP_Text CalenderDay;
+    public TMP_Text ExerciseCount;
+    public TMP_Text KcalCount;
+    public TMP_Text MinutesCount;
+    private ProfileGetManager profileGetManager;
+    private CalenderImage lastSelectedDate;
+
+    private float ExerciseCountNum = 0;
+    private float KcalCountNum = 0;
+    private float MinutesCountNum = 0;
+    private float NowExerciseCountNum = 0;
+    private float NowKcalCountNum = 0;
+    private float NowMinutesCountNum = 0;
+
+    private void Start()
+    {
+        profileGetManager = FindObjectOfType<ProfileGetManager>();
+        if (profileGetManager == null)
+        {
+            Debug.LogError("ProfileGetManager not found in the scene!");
+        }
+    }
+
+    public void OnDateSelected(CalenderImage selectedDate)
+    {
+        if (lastSelectedDate != null)
+        {
+            lastSelectedDate.SetBold(false);
+        }
+
+        selectedDate.SetBold(true);
+        lastSelectedDate = selectedDate;
+
+        JSWSoundManager.Get().PlayEftSound(JSWSoundManager.ESoundType.EFT_PROFILESCENE);
+
+        string dateString = selectedDate.GetDateString();
+        CalenderDay.text = dateString;
+
+        profileGetManager.SendDataToServer(dateString, false);
+        StartCoroutine(WaitForDataAndUpdateUI());
+
+        // Trigger profile UI animation
+        GameObject profile = GameObject.Find("Profile");
+        if (profile != null)
+        {
+            Animator animator = profile.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.enabled = true;
+                animator.CrossFade("ProfileUIUI", 0);
+            }
+        }
+    }
+
+    private IEnumerator WaitForDataAndUpdateUI()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        // Reset current values to start new interpolation
+        NowExerciseCountNum = 0;
+        NowKcalCountNum = 0;
+        NowMinutesCountNum = 0;
+
+        // Update target values from AvatarInfo
+        ExerciseCountNum = AvatarInfo.instance.totalExerciseCount;
+        KcalCountNum = (float)AvatarInfo.instance.totalCaloriesBurned;
+        MinutesCountNum = ExerciseCountNum / 2;
+
+        // Start coroutine for smooth value transition
+        StartCoroutine(SmoothValueTransition());
+    }
+
+    private IEnumerator SmoothValueTransition()
+    {
+        float elapsedTime = 0;
+        float transitionDuration = 1f; // Adjust this value to change the speed of the transition
+
+        while (elapsedTime < transitionDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / transitionDuration;
+
+            NowExerciseCountNum = Mathf.Lerp(0, ExerciseCountNum, t);
+            NowKcalCountNum = Mathf.Lerp(0, KcalCountNum, t);
+            NowMinutesCountNum = Mathf.Lerp(0, MinutesCountNum, t);
+
+            UpdateUIText();
+
+            yield return null;
+        }
+
+        // Ensure final values are set
+        NowExerciseCountNum = ExerciseCountNum;
+        NowKcalCountNum = KcalCountNum;
+        NowMinutesCountNum = MinutesCountNum;
+        UpdateUIText();
+    }
+
+    private void UpdateUIText()
+    {
+        ExerciseCount.text = ((int)NowExerciseCountNum).ToString();
+        KcalCount.text = ((int)NowKcalCountNum).ToString();
+        MinutesCount.text = ((int)NowMinutesCountNum).ToString();
+    }
+}
